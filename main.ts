da49640ceb66def4b5a271615dd15ba2b6c2baab
IsobelPaes <95632385+IsobelPaes@users.noreply.github.com>
2021-12-06 14:25:10 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,46 @@
+radio.onReceivedValue(function (name, value) {
+    if (name == "Go") {
+        bitbot.setLedColor(0x40C0FF)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . . # . .
+            . . # . .
+            `)
+        bitbot.goms(BBDirection.Forward, 30, value)
+    } else if (name == "Rotate") {
+        bitbot.setLedColor(0x40C0FF)
+        basic.showLeds(`
+            . . # . .
+            # # # # .
+            # . # . .
+            # . . . .
+            # # # # #
+            `)
+        bitbot.rotatems(BBRobotDirection.Right, 30, value)
+    } else {
+        basic.showIcon(IconNames.No)
+        bitbot.setLedColor(0xFF0000)
+        basic.pause(500)
+    }
+    basic.showLeds(`
+        # # # . .
+        # . # . .
+        # # . . .
+        # # . . .
+        # . # . .
+        `)
+    bitbot.setLedColor(0x659900)
+})
+bitbot.select_model(BBModel.Classic)
+radio.setGroup(99)
+basic.showLeds(`
+    # # # . .
+    # . # . .
+    # # . . .
+    # # . . .
+    # . # . .
+    `)
+bitbot.setLedColor(0x659900)
+bitbot.ledBrightness(40)
